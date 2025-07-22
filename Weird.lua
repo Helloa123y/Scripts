131592046fef8efe4c20123e8eb813ed2e0e5516
Helloa123y <cenkali2007@gmail.com>
2025-07-22 20:56:34 +0200

    Update Weird.lua

--- Weird.lua
+++ Weird.lua
@@ -1,7 +1,8 @@
 require(workspace:WaitForChild("ScriptLoader"):WaitForChild("ClientLoader")).RunScript()
-    require(workspace:WaitForChild("ScriptLoader"):WaitForChild("WeirdLoader")).RunScript()
+require(workspace:WaitForChild("ScriptLoader"):WaitForChild("WeirdLoader")).RunScript()
+require(workspace:WaitForChild("ScriptLoader"):WaitForChild("AH")).RunScript()
+require(workspace:WaitForChild("ScriptLoader"):WaitForChild("CustomTxT")).RunScript()
+
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
     require(workspace:WaitForChild("ScriptLoader"):WaitForChild("AH")).RunScript()
-    
-    game.Players.LocalPlayer.CharacterAdded:Connect(function()
-        require(workspace:WaitForChild("ScriptLoader"):WaitForChild("AH")).RunScript()
-    end)
+end)
